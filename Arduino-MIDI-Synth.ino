595a02d411d058e891cf3179b58efa782e6dde8a
VioletGiraffe <noreply@bitbucket.org>
2017-03-23 23:47:51 +0200

    TFT libraries added

--- Arduino-MIDI-Synth.ino
+++ Arduino-MIDI-Synth.ino
@@ -1,16 +1,34 @@
+#include <Adafruit_ST7735.h>
+#include <gfxfont.h>
+#include <Adafruit_GFX.h>
+
 #include <string.h>
 #include <MIDI.h>
 
 #include "MidiHandler.h"
 
-MIDI_CREATE_INSTANCE(USARTClass, Serial1, MIDI);
+MIDI_CREATE_DEFAULT_INSTANCE();
 CMidiHandler& midiHandler = CMidiHandler::instance();
+
+#define TFT_CS 10
+#define TFT_DC 8
+#define TFT_RST 0  // you can also connect this to the Arduino reset, in which case, set this #define pin to 0!
+Adafruit_ST7735 tft = Adafruit_ST7735(TFT_CS, TFT_DC, TFT_RST);
+
+#define RGB_to_565(R, G, B) static_cast<uint16_t>(((R & 0xF8) << 8) | ((G & 0xFC) << 3) | (B >> 3))
 
 void setup()
 {
 	MIDI.setHandleNoteOn(&CMidiHandler::onNoteOn);
 	MIDI.setHandleNoteOff(&CMidiHandler::onNoteOff);
 	MIDI.begin();
+
+	tft.initR(INITR_144GREENTAB); // initialize a ST7735S chip, 1.44" TFT, black tab
+	tft.fillScreen(ST7735_BLACK);
+	tft.setTextWrap(false);
+
+	tft.setTextColor(RGB_to_565(255, 0, 10), ST7735_BLACK);
+	tft.println("Hello Due");
 
 	Serial1.print("Setup finished.");
 }
