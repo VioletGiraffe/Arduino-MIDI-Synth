ff39167aa9d99cea8fef338042ad66edaeda077d
VioletGiraffe <VioletGiraffe@users.noreply.github.com>
2017-03-27 16:57:40 +0300

    FixedPoint expanded

--- FixedPoint.h
+++ FixedPoint.h
@@ -8,11 +8,11 @@ class FixedPoint
 public:
 	static const int FractionalBits = sizeof(BaseType) * 8 - IntegerBits;
 
-	FixedPoint() : _value(0) {}
-
 	// TODO: template method for any integer arguments - possibly not doable on embedded systems due to lack of <type_traits> and std::enable_if
 	FixedPoint(BaseType integerValue) : _value(integerValue << FractionalBits) {}
 	FixedPoint(float fpValue) : _value(static_cast<BaseType>(fpValue * (1 << FractionalBits) /* 2^FractionalBits */) + 0.5f) {}
+
+// Basic aritmetic operations
 
 	FixedPoint operator+(const FixedPoint& other) const
 	{
@@ -50,6 +50,56 @@ public:
 		return result;
 	}
 
+	FixedPoint& operator*=(const FixedPoint& other)
+	{
+		typename detail::expand_type<BaseType>::type intermediateResult = _value;
+		intermediateResult *= other._value;
+
+		_value = (BaseType)(intermediateResult >> FractionalBits);
+		return *this;
+	}
+
+	FixedPoint operator-() const
+	{
+		FixedPoint result;
+		result._value = -_value;
+		return result;
+	}
+
+// Comparion
+
+	bool operator<(const FixedPoint& other) const
+	{
+		return _value < other._value;
+	}
+
+	bool operator<=(const FixedPoint& other) const
+	{
+		return _value <= other._value;
+	}
+
+	bool operator>(const FixedPoint& other) const
+	{
+		return _value > other._value;
+	}
+
+	bool operator>=(const FixedPoint& other) const
+	{
+		return _value >= other._value;
+	}
+
+	bool operator==(const FixedPoint& other) const
+	{
+		return _value == other._value;
+	}
+
+	bool operator!=(const FixedPoint& other) const
+	{
+		return _value != other._value;
+	}
+
+// Conversions
+
 	BaseType toInt() const
 	{
 		return _value >> FractionalBits;
@@ -61,6 +111,10 @@ public:
 		static const BaseType fractMask = (1 << FractionalBits) - 1;
 		return (float)toInt() + (_value & fractMask) * fractMultiplier;
 	}
+
+
+private:
+	FixedPoint() = default;
 
 private:
 	BaseType _value;
