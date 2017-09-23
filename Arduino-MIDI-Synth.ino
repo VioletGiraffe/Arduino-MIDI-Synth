49fb603ffbe89602d96be40c16d0ed4c7967ab6e
VioletGiraffe <noreply@bitbucket.org>
2017-09-24 00:45:22 +0300

    Rotary encoder skeleton handler added

--- Arduino-MIDI-Synth.ino
+++ Arduino-MIDI-Synth.ino
@@ -10,6 +10,7 @@
 #include "FixedPoint.h"
 #include "WaveformSin.h"
 #include "Generator.h"
+#include "QuadratureRotaryEncoder\QuadratureRotaryEncoder.h"
 
 MIDI_CREATE_DEFAULT_INSTANCE();
 CMidiHandler& midiHandler = CMidiHandler::instance();
@@ -24,16 +25,23 @@ Adafruit_ST7735 tft = Adafruit_ST7735(TFT_CS, TFT_DC, TFT_RST);
 CGenerator soundGen;
 constexpr uint32_t samplingRate = 24000;
 
+#define ROT_ENC_PIN_A 11
+#define ROT_ENC_PIN_B 12
+
+QuadratureRotaryEncoder encoder(ROT_ENC_PIN_A, ROT_ENC_PIN_B);
+
 void playSound()
 {
 	dac_write(soundGen.nextSample<CWaveformSin, samplingRate>(1000u));
 }
 
+int position = 0;
+
 void setup()
 {
-//	Serial.begin(115200);
 	//MIDI.setHandleNoteOn(&CMidiHandler::onNoteOn);
 	//MIDI.setHandleNoteOff(&CMidiHandler::onNoteOff);
+
 	MIDI.setHandleNoteOn([](byte channel, byte note, byte velocity) {
 		tft.setTextSize(3);
 		tft.fillScreen(ST7735_BLACK);
@@ -67,31 +75,33 @@ void setup()
 	pinMode(LED_BUILTIN, OUTPUT);
 	digitalWrite(LED_BUILTIN, LOW);
 
+	encoder.setOnRotationListener([](QuadratureRotaryEncoder::RotationDirection direction) {});
+
+	Timer2.attachInterrupt([](){encoder.update();}).setFrequency(2000).start();
+
 	dac_setup();
 
-	Timer2.attachInterrupt(&playSound).setFrequency(samplingRate).start(); // 24 kHz sampling rate
-
-//	Serial.println("Setup finished.");
+	Timer3.attachInterrupt(&playSound).setFrequency(samplingRate).start(); // 24 kHz sampling rate
 }
 
-void dac_setup ()
+void dac_setup()
 {
-  pmc_enable_periph_clk (DACC_INTERFACE_ID) ; // start clocking DAC
-  DACC->DACC_CR = DACC_CR_SWRST ;  // reset DAC
+	pmc_enable_periph_clk(DACC_INTERFACE_ID); // start clocking DAC
+	DACC->DACC_CR = DACC_CR_SWRST;  // reset DAC
 
-  DACC->DACC_MR =
-    0 /* No trigger, else DACC_MR_TRGEN_EN */ | DACC_MR_TRGSEL (0) |
-    (0 << DACC_MR_USER_SEL_Pos) |  // select channel 0
-    DACC_MR_REFRESH (0x2) |        // Refresh = DAC_CLOCK / (1024 * Fs) (https://cjpnmiscellany.wordpress.com/2015/08/20/settings-for-the-arduino-dac/)
-    (24 << DACC_MR_STARTUP_Pos) ;  // 24 = 1536 cycles which I think is in range 23..45us since DAC clock = 42MHz
+	DACC->DACC_MR =
+		0 /* No trigger, else DACC_MR_TRGEN_EN */ | DACC_MR_TRGSEL(0) |
+		(0 << DACC_MR_USER_SEL_Pos) |  // select channel 0
+		DACC_MR_REFRESH(0x2) |        // Refresh = DAC_CLOCK / (1024 * Fs) (https://cjpnmiscellany.wordpress.com/2015/08/20/settings-for-the-arduino-dac/)
+		(24 << DACC_MR_STARTUP_Pos);  // 24 = 1536 cycles which I think is in range 23..45us since DAC clock = 42MHz
 
-  DACC->DACC_IDR = 0xFFFFFFFF ; // no interrupts
-  DACC->DACC_CHER = DACC_CHER_CH0 << 0 ; // enable chan0
+	DACC->DACC_IDR = 0xFFFFFFFF; // no interrupts
+	DACC->DACC_CHER = DACC_CHER_CH0 << 0; // enable chan0
 }
 
-inline void dac_write (int val)
+inline void dac_write(int val)
 {
-  DACC->DACC_CDR = val & 0xFFF ;
+	DACC->DACC_CDR = val & 0xFFF;
 }
 
 void loop()
---
+++ QuadratureRotaryEncoder/QuadratureRotaryEncoder.cpp
@@ -0,0 +1,26 @@
+#include "QuadratureRotaryEncoder.h"
+
+#include <Arduino.h>
+
+QuadratureRotaryEncoder::QuadratureRotaryEncoder(int pinA, int pinB)
+{
+	pinMode(pinA, INPUT_PULLUP);
+	pinMode(pinB, INPUT_PULLUP);
+
+	_debouncerA.attach(pinA);
+	_debouncerA.interval(4);
+
+	_debouncerB.attach(pinB);
+	_debouncerB.interval(4);
+}
+
+void QuadratureRotaryEncoder::update()
+{
+	_debouncerA.update();
+	_debouncerB.update();
+
+	if (_debouncerA.fell())
+	{
+		_listener(_debouncerB.read() != false ? CW : CCW);
+	}
+}
